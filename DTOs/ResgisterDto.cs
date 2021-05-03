f04ac52cb795dfe9903103e724524af959bc26a1
Muhammad Hammad <muhammadhammad.94@gmail.com>
2021-05-03 14:00:20 +0500

    First Commit

---
+++ DTOs/ResgisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class ResgisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
